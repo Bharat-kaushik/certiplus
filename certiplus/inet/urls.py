311f6e458e441025d59ac27f79eab2159b6df069
bienbharat <bharats@bientechnologies.com>
2021-03-24 20:55:05 +0530

    not much

--- certiplus/inet/urls.py
+++ certiplus/inet/urls.py
@@ -5,6 +5,6 @@ from inet.views import home,base,Grow_with_us
 urlpatterns = [
     path('basic',base.basic),
     path('',home.Home),
-    path('SubGrow/<int:section>',Grow_with_us.SubGrow)
+    path('SubGrow/<int:section>', Grow_with_us.SubGrow)
 
 ]
