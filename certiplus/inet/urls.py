1dd6029d181e8c50b9cc341e2a7120256625eb3f
Divin kumar <divinkumar22@gmail.com>
2021-03-24 20:48:51 +0530

    dynamic all category

--- certiplus/inet/urls.py
+++ certiplus/inet/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
 from . import views
-from inet.views import home,base,Grow_with_us
+from inet.views import home,base,Grow_with_us,subgrowdetails, details_section_view
 
 urlpatterns = [
     path('basic',base.basic),
-    path('',home.Home),
-    path('SubGrow/<int:section>',Grow_with_us.SubGrow)
+    path('',details_section_view.fetch_category_items),
+    path('SubGrow/<int:section>',Grow_with_us.SubGrow),
+    path('SubGrowdetails/<int:sub_section>',subgrowdetails.SubGrowDetails)
 
 ]
